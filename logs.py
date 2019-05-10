e3deb19c7b09e99e158553cc3327b17af6ce6787
Art-Lubinski <50474900+Art-Lubinski@users.noreply.github.com>
2019-05-10 00:48:51 -0400

    Add files via upload

---
+++ logs.py
@@ -0,0 +1,48 @@
+import os
+import datetime
+import sys
+
+current_dir = os.path.realpath(__file__)
+
+folder_path = current_dir[:-8] + "/Logs"
+logs_path = folder_path + "/logs.txt"
+errors_path = folder_path + "/errors.txt"
+statistics_path = folder_path + "/statistics.txt"
+daily_sales_path = folder_path + "/daily_sales.txt"
+
+if sys.platform == "win32":
+    folder_path = folder_path.replace("/","\\")
+    logs_path = logs_path.replace("/","\\")
+    errors_path = errors_path.replace("/","\\")
+    statistics_path = statistics_path.replace("/","\\")
+    daily_sales_path= daily_sales_path.replace("/", "\\")
+
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+logs = open(logs_path, "a")
+errors = open(errors_path, "a")
+statistics = open(statistics_path, "a")
+daily_sales = open(daily_sales_path, "a")
+
+
+def write_to_log(message):
+    timestamp = datetime.datetime.now() - datetime.timedelta(days=1)
+    message = str(timestamp.date()) + " " +  message + "\n"
+    logs.write(message)
+
+def write_to_errors(message):
+    timestamp = datetime.datetime.now() - datetime.timedelta(days=1)
+    message = str(timestamp.date()) + " " + message + "\n"
+    errors.write(message)
+
+def write_to_daily_sales(message):
+    timestamp = datetime.datetime.now() - datetime.timedelta(days=1)
+    message = str(timestamp.date()) + " " + message + "\n"
+    daily_sales.write(message)
+
+def write_to_statistics(message):
+    timestamp = datetime.datetime.now() - datetime.timedelta(days=1)
+    message = str(timestamp.date()) + " " + message + "\n"
+    statistics.write(message)
---
+++ reports.py
@@ -0,0 +1,62 @@
+import os
+import datetime
+import re
+
+current_dir = os.path.realpath(__file__)
+folder_path = current_dir[:-11] + "/Logs"
+daily_sales_path = folder_path + "/daily_sales.txt"
+
+
+def get_sales_report(from_date, to_date):
+    daily_sales = open(daily_sales_path, "r")
+    table_purchase_4g, table_renewed_4g = _sales_report(daily_sales, from_date, to_date, "^4G-.*$")
+    daily_sales = open(daily_sales_path, "r")
+    table_purchase_dsl, table_renewed_dsl = _sales_report(daily_sales, from_date, to_date, "DSL-.*$")
+    return table_purchase_4g, table_renewed_4g, table_purchase_dsl, table_renewed_dsl
+
+
+def _sales_report(daily_sales, from_date, to_date, regex):
+    table_purchase = [
+        ['trial',0,0],
+        ['weekly',0,0],
+        ['bi-weekly',0,0],
+        ['monthly',0,0,],
+    ]
+    table_renewed = [
+        ['weekly',0, 0,],
+        ['biweekly',0, 0,],
+        ['monthly', 0, 0,],
+    ]
+    for x in daily_sales:
+        a = x.split()
+        if len(a) != 0:
+            b = datetime.datetime.strptime(a[0], '%Y-%m-%d').date()
+            if from_date <= b <= to_date:
+                if re.findall(regex, a[1]):
+                    if a[5] == "purchase":
+                        if a[3] == "trial":
+                            table_purchase[0][1] = table_purchase[0][1] + int(a[4])
+                            table_purchase[0][2] = table_purchase[0][2] + 1
+                        elif a[3] == "weekly":
+                            table_purchase[1][1] = table_purchase[1][1] + int(a[4])
+                            table_purchase[1][2] = table_purchase[1][2] + 1
+
+                        elif a[3] == "biweekly":
+                                table_purchase[2][1] = table_purchase[2][1] + int(a[4])
+                                table_purchase[2][2] = table_purchase[2][2] + 1
+                        elif a[3] == "monthly":
+                                table_purchase[3][1] = table_purchase[3][1] + int(a[4])
+                                table_purchase[3][2] = table_purchase[3][2] + 1
+                    else:
+                        if a[3] == "weekly":
+                            table_renewed[0][1] = table_renewed[0][1] + int(a[4])
+                            table_renewed[0][2] = table_renewed[0][2] + 1
+                        elif a[3] == "biweekly":
+                            table_renewed[1][1] = table_renewed[1][1] + int(a[4])
+                            table_renewed[1][2] = table_renewed[1][2] + 1
+                        elif a[3] == "monthly":
+                            table_renewed[2][1] = table_renewed[2][1] + int(a[4])
+                            table_renewed[2][2] = table_renewed[2][2] + 1
+
+    return table_purchase, table_renewed
+
